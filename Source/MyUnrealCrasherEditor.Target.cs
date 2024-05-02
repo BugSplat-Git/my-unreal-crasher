36e27882c492666af1e97dd76b8d1842d2abe594
Bobby Galli <bobbyg603@pm.me>
2024-05-02 08:05:06 -0400

    fix: build for 5.4

--- Source/MyUnrealCrasherEditor.Target.cs
+++ Source/MyUnrealCrasherEditor.Target.cs
@@ -8,7 +8,7 @@ public class MyUnrealCrasherEditorTarget : TargetRules
 	public MyUnrealCrasherEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 		ExtraModuleNames.AddRange( new string[] { "MyUnrealCrasher" } );
 	}
 }
