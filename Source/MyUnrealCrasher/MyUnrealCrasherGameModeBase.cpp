3c92b5b05abbfceb817a91a4b6bbf212170c2faf
Bobby Galli <bobbyg603@pm.me>
2026-04-08 15:18:14 -0400

    fix: use bugsplat setattributes function

--- Source/MyUnrealCrasher/MyUnrealCrasherGameModeBase.cpp
+++ Source/MyUnrealCrasher/MyUnrealCrasherGameModeBase.cpp
@@ -1,7 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "MyUnrealCrasherGameModeBase.h"
-#include "GenericPlatform/GenericPlatformCrashContext.h"
+#include "BugSplatAttributes.h"
 
 AMyUnrealCrasherGameModeBase::AMyUnrealCrasherGameModeBase()
 {
@@ -9,13 +9,13 @@ AMyUnrealCrasherGameModeBase::AMyUnrealCrasherGameModeBase()
 	UE_LOG(LogTemp, Log, TEXT("MyUnrealCrasherGameModeBase is initializing!"));
 
 	// BugSplat Key field - used to display different flavors of the support response page at crash time
-	FGenericCrashContext::SetGameData(TEXT("BugSplatApplicationKey"), TEXT("en-US"));
+	UBugSplatAttributes::SetAttribute(TEXT("BugSplatApplicationKey"), TEXT("en-US"));
 
 	// BugSplat Notes field - used for arbitrary extra crash context
-	FGenericCrashContext::SetGameData(TEXT("BugSplatNotes"), TEXT("Development Build"));
+	UBugSplatAttributes::SetAttribute(TEXT("BugSplatNotes"), TEXT("Development Build"));
 
-	// GameData values will be parsed as Attributes by BugSplat
-	FGenericCrashContext::SetGameData(TEXT("CurrentWorld"), TEXT("Alpha Centari"));
-	FGenericCrashContext::SetGameData(TEXT("GamePads"), TEXT("1"));
-	FGenericCrashContext::SetGameData(TEXT("IsExternalQABuild"), TEXT("true"));
+	// Custom attributes will be included in crash reports on all platforms
+	UBugSplatAttributes::SetAttribute(TEXT("CurrentWorld"), TEXT("Alpha Centari"));
+	UBugSplatAttributes::SetAttribute(TEXT("GamePads"), TEXT("1"));
+	UBugSplatAttributes::SetAttribute(TEXT("IsExternalQABuild"), TEXT("true"));
 }
